4bb29f27e3bb1f89fb48fd1d3d1634a0b3257cad
eguzhova <81353538+eguzhova@users.noreply.github.com>
2021-04-07 16:25:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        int currentBalance = 100_00;
+        int summAdded = 1100_75;
+
+        if (summAdded > 1000_00) {
+            int bonusStep = 100_00;
+            int bonusAmmount = summAdded / bonusStep * 100;
+            currentBalance += summAdded + bonusAmmount;
+        }
+        else currentBalance = currentBalance + summAdded;
+        int balanceRuble = currentBalance / 100;
+        int balanceCents = currentBalance % 100;
+        System.out.println("Баланс после пополнения: "+balanceRuble+" руб. "+balanceCents+" коп.");
+    }
+}
